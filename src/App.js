8f5e94c4fb69e4e2a80740eba94c2ac3906c898e
Adam Eastwood <adam-3@live.co.uk>
2023-02-05 04:31:50 +0000

    fixed links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {Route, Routes} from 'react-router-dom';
 import Main from "./components/main/main";
-import Media from "./components/maps/maps";
-import Maps from "./components/media/media";
+import Media from "./components/media/media";
+import Maps from "./components/maps/maps";
 
 function App() {
     return <Routes>
